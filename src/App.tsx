f37fe43df9b59ddd5e834f93eba4a829cc661b4b
Helton Batista <heltonbts@icloud.com>
2025-07-25 11:40:03 -0300

    feat: authenticate verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import Login from './pages/Auth/Login'
 import SignUp from './pages/Auth/SignUp'
-import Home from './pages/Auth/SignUp'
+import Home from './pages/Home/Home'
 
 function App() {
+  const Root = () => {
+    const isAuthenticate = !!localStorage.getItem('cm:token')
+
+    return isAuthenticate ? <Navigate to="/home" /> : <Navigate to="/login" />
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Root />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
